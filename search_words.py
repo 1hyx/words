88a8ef2c4678feb9bca694745eb593d4e3afda97
rene0410 <hyx552211@163.com>
2019-10-18 18:01:04 +0800

    base

--- search_words.py
+++ search_words.py
@@ -10,4 +10,32 @@
 """
 
 import pandas as pd
+import os
+import subprocess
+import urllib.request as request
 
+
+# 测试网络
+def test_network():
+    null = open(os.devnull, 'w')
+    return1 = subprocess.call('ping 8.8.8.8', shell=True, stdout=null, stderr=null)
+    if return1 == 0:
+        subprocess.call('msdt.exe /id NetworkDiagnosticsNetworkAdapter', shell=True, stdout=null, stderr=null)
+        print('网络有问题，请保证网络通畅！')
+        null.close()
+        return 0
+    else:
+        null.close()
+        return 1
+
+
+def get_words_meaning():
+    base_url = 'http://iciba.com/'
+    with request.urlopen('http://python.org/') as response:
+        html = response.read()
+        print(html)
+
+
+if __name__ == '__main__':
+    # test_network()
+    get_words_meaning()
